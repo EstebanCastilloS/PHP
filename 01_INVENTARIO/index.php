2ccdd4d73810c03720e5eb17457b7fd4be7c49a8
EstebanCastillo <ncastillo308@unab.edu.co>
2023-11-20 23:13:58 -0500

    Integrando Estilos al proyecto

--- 01_INVENTARIO/index.php
+++ 01_INVENTARIO/index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema Inventario</title>
+    <?php include 'inc/head.php'; ?>
 </head>
 <body>
+
+<?php 
+
+    include 'inc/navbar.php';
+
+    // include 'inc/jumbotron.php';
+
+    // include 'inc/cards.php';
+
+    // include 'inc/footer.php';
+
+?>
     
 </body>
 </html>
