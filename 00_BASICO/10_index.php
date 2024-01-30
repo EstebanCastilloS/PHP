30f3d187d6e9456752ecd3af28e34a84b0f63969
EstebanCastillo <ncastillo308@unab.edu.co>
2024-01-30 15:46:28 -0500

    10_index

---
+++ 00_BASICO/10_index.php
@@ -0,0 +1,13 @@
+<?php
+
+// $nombre = $_POST['nombre'];
+// $asignatura = $_POST['asignatura'];
+// $frutas = $_POST['frutas'];
+
+// echo $nombre." - ".$asignatura." - ".$frutas;
+
+$nombre = $_GET['nombre'];
+$asignatura = $_GET['asignatura'];
+$frutas = $_GET['frutas'];
+
+echo $nombre." - ".$asignatura." - ".$frutas;
